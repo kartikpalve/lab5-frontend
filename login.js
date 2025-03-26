6e4dda89a6f5b7e4d0ef67618de33eac6d036ec5
Kartik Mahadeo Palve <120277260+kartikpalve@users.noreply.github.com>
2025-03-26 12:41:42 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 // Firebase configuration - replace with your own config
 const firebaseConfig = {
-    apiKey: "AIzaSyDZmtXW8JjPzu-OYZLzzRNIpeiWW6BO1Qg",
-    authDomain: "lab5-afa1c.firebaseapp.com",
-    projectId: "lab5-afa1c",
-    storageBucket: "lab5-afa1c.appspot.com",
-    messagingSenderId: "428223976034",
-    appId: "1:428223976034:web:a898613a17cb550752f73b"
+    apiKey: "AIzaSyAhEGC0bxwRKEbRFQFc99FAzYhL2rhWhFg",
+    authDomain: "astute-impulse-447907-u1.firebaseapp.com",
+    projectId: "astute-impulse-447907-u1",
+    storageBucket: "astute-impulse-447907-u1.firebasestorage.app",
+    messagingSenderId: "531778193935",
+    appId: "1:531778193935:web:4125d2c951dc35df7ff17a"
   };
   
   // Initialize Firebase
